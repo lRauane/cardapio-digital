57cebe573eeb1504dda4e35c7d9b912d02785ff6
Rauane Lima <rauanelima7@gmail.com>
2023-04-14 14:40:57 -0300

    New feature: Estilizando cards

---
+++ src/components/Card.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Product1 from '../assets/product-1.svg'
+import { BiCart, BiHeart } from 'react-icons/bi'
+
+const Card = () => {
+  return (
+    <div className="group relative">
+      <div className="min-h-80 aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md lg:aspect-none relative">
+        <img src={Product1} alt='' className="object-center lg:h-full lg:w-full" />
+        <div className="absolute top-3 left-2 bg-zinc-800 rounded-sm p-2  hover:bg-zinc-900/80 ">
+          <BiHeart size={32} color='white'/>
+        </div>
+      </div>
+      <div className="mt-2 gap-2 flex flex-col">
+        <div>
+          <h3 className="text-3xl pb-2 font-bold text-gray-700">
+            Pastel
+          </h3>
+          <div className='border-b w-[300px] lg:w-[250px] border-solid border-gray-400'></div>
+        </div>
+        <div className='flex items-center justify-around'>
+          <p className="mt-1 text-base md:text-lg text-gray-500">R$ 15.00</p>
+          <a href='' className='bg-zinc-800 rounded-sm p-3 hover:bg-zinc-900/80 md:p-2'><BiCart color='white' size={32} /></a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -1,124 +1,23 @@
 import React from 'react';
-import CardImage from './CardImage';
-import Product1 from '../assets/product-1.svg'
+import Card from './Card';
 
 const CardProduct = () => {
   return (
     <div className="bg-white">
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-      
-
+      <div className="mx-auto max-w-2xl px-4 py-1 sm:px-6 sm:py-24 lg:max-w-6xl ">
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-
-          <div className="group relative">
-            <div className="min-h-80 aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-              <img
-                src={Product1}
-                alt=''
-                className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-              />
-            </div>
-            <div className="mt-4 flex justify-between">
-              <div>
-                <h3 className="text-sm text-gray-700">
-                  <a href='#'>
-                    <span aria-hidden="true" className="absolute inset-0" />
-                    Pastel
-                  </a>
-                </h3>
-                <p className="mt-1 text-sm text-gray-500">ttttttttt</p>
-              </div>
-              <p className="text-sm font-medium text-gray-900">35</p>
-            </div>
-          </div>
-          <div className="group relative">
-            <div className="min-h-80 aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-              <img
-                src={Product1}
-                alt=''
-                className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-              />
-            </div>
-            <div className="mt-4 flex justify-between">
-              <div>
-                <h3 className="text-sm text-gray-700">
-                  <a href='#'>
-                    <span aria-hidden="true" className="absolute inset-0" />
-                    Pastel
-                  </a>
-                </h3>
-                <p className="mt-1 text-sm text-gray-500">ttttttttt</p>
-              </div>
-              <p className="text-sm font-medium text-gray-900">35</p>
-            </div>
-          </div>
-          <div className="group relative">
-            <div className="min-h-80 aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-              <img
-                src={Product1}
-                alt=''
-                className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-              />
-            </div>
-            <div className="mt-4 flex justify-between">
-              <div>
-                <h3 className="text-sm text-gray-700">
-                  <a href='#'>
-                    <span aria-hidden="true" className="absolute inset-0" />
-                    Pastel
-                  </a>
-                </h3>
-                <p className="mt-1 text-sm text-gray-500">ttttttttt</p>
-              </div>
-              <p className="text-sm font-medium text-gray-900">35</p>
-            </div>
-          </div>
-          <div className="group relative">
-            <div className="min-h-80 aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-              <img
-                src={Product1}
-                alt=''
-                className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-              />
-            </div>
-            <div className="mt-4 flex justify-between">
-              <div>
-                <h3 className="text-sm text-gray-700">
-                  <a href='#'>
-                    <span aria-hidden="true" className="absolute inset-0" />
-                    Pastel
-                  </a>
-                </h3>
-                <p className="mt-1 text-sm text-gray-500">ttttttttt</p>
-              </div>
-              <p className="text-sm font-medium text-gray-900">35</p>
-            </div>
-          </div>
-          <div className="group relative">
-            <div className="min-h-80 aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-              <img
-                src={Product1}
-                alt=''
-                className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-              />
-            </div>
-            <div className="mt-4 flex justify-between">
-              <div>
-                <h3 className="text-sm text-gray-700">
-                  <a href='#'>
-                    <span aria-hidden="true" className="absolute inset-0" />
-                    Pastel
-                  </a>
-                </h3>
-                <p className="mt-1 text-sm text-gray-500">ttttttttt</p>
-              </div>
-              <p className="text-sm font-medium text-gray-900">35</p>
-            </div>
-          </div>
-
+         <Card />
+         <Card />
+         <Card />
+         <Card />
+         <Card />
+         <Card />
+         <Card />
+         <Card />
         </div>
       </div>
     </div>
+
   );
 }
 
