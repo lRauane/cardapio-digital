36e35588bf23433e92c68d27a4cdffb6e7494721
Rauane Lima <rauanelima7@gmail.com>
2023-04-20 10:18:19 -0300

    Deixando componente de pesquisa quase pronto

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BiSearch } from 'react-icons/bi'
 import api from '../services/api';
 
 const Input = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [isTyping, setIsTyping] = useState(false);
 
-  async function searchRecepies(event) {
+  async function searchRecipes(event) {
     event.preventDefault();
+
+    if (!searchTerm) {
+      setSearchResults([]);
+      return;
+    }
+
     const response = await api.get("/foods")
     const recipes = response.data;
 
@@ -16,39 +23,58 @@ const Input = () => {
     )
 
     setSearchResults(results)
+    setIsTyping(false);
   }
+
+  useEffect(() => {
+    if (searchTerm !== '') {
+      setIsTyping(true);
+      searchRecipes();
+    } else {
+      setSearchResults([]);
+      setIsTyping(false);
+    }
+  }, [searchTerm]);
 
   return (
     <div>
-      <form onSubmit={searchRecepies} className='flex items-center'>
+      <form onSubmit={searchRecipes} className="flex items-center">
         <input
           type="text"
           value={searchTerm}
-          onChange={event => setSearchTerm(event.target.value)}
-          placeholder='Pesquise por um produto'
-          className='p-3 md:p-4 md:w-[300px] w-[100px] border border-gray-300 border-solid border-opacity-50 rounded-tl-sm rounded-bl-sm  outline-none text-base hidden md:block' />
+          onChange={(event) => {
+            setSearchTerm(event.target.value);
+            setIsTyping(true);
+          }}
+          placeholder="Pesquise por um produto"
+          className="p-3 md:p-4 md:w-[300px] w-[100px] border border-gray-300 border-solid border-opacity-50 rounded-tl-sm rounded-bl-sm outline-none text-base hidden md:block"
+        />
         <button
-          type='submit'
-          className='bg-zinc-800 p-3 md:p-4 rounded-tr-sm rounded-br-sm hover:bg-zinc-900/80 hidden md:block '>
-          <BiSearch size={27} color='white' />
+          type="submit"
+          className="bg-zinc-800 p-3 md:p-4 rounded-tr-sm rounded-br-sm hover:bg-zinc-900/80 hidden md:block"
+        >
+          <BiSearch size={27} color="white" />
         </button>
       </form>
-
-      {searchResults.length > 0 ? (
+      {isTyping && (
+        <p className="bg-white w-[400px] rounded-lg text-blck shadow-md shadow-black/40  p-4 mb-4 mt-2 absolute z-50">Procurando resultados...</p>
+      )}
+      
+      {!isTyping && searchResults.length > 0 ? (
         searchResults.map(recipe => (
-          <button key={recipe.id} className="bg-zinc-900 w-[400px] rounded-lg text-white shadow-md p-4 mb-4 absolute z-50">
+          <div key={recipe.id} className="bg-white w-[50%]  rounded-lg text-black absolute shadow-md shadow-black/40 p-4 mb-4 z-50">
             <h2 className="text-lg  font-bold">{recipe.name}</h2>
-            <p className="text-white">{recipe.description}</p>
+            <p className="text-black">{recipe.description}</p>
             <div className="mt-2 flex">
               <span className="text-gray-600 mr-2">{recipe.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</span>
             </div>
-          </button>
+          </div>
         ))
       ) : (
-        searchTerm && <p className="text-gray-600 absolute top-full left-0 w-full bg-white rounded-lg shadow-md p-4">{`Nenhuma receita encontrada para "${searchTerm}".`}</p>
+        !isTyping && searchTerm && <p className="bg-zinc-900 w-[400px] rounded-lg text-white shadow-md p-4 mb-4
+          absolute z-50">Nenhum resultado encontrado</p>
       )}
     </div>
-
   );
 }
 
