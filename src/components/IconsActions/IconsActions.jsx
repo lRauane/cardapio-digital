63509f1f7e428206077f2e52e107038cba7f9ef8
Rauane Lima <rauanelima7@gmail.com>
2023-05-09 11:32:44 -0300

    Ajustando componente de produtos

--- src/components/IconsActions/IconsActions.jsx
+++ src/components/IconsActions/IconsActions.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import CartButton from '../Dialog/CartButton';
-import FavoriteButton from '../Dialog/FavoriteButton';
 
 const IconsActions = () => {
   return (
     <div
       className=' flex gap-2 md:gap-5'>
-      <FavoriteButton />
       <CartButton />
     </div>
   );
